9869dc80795bd71cc73b6cf2e8e3d63b93d0a605
Omar Droubi <omardroubi@live.com>
2020-04-26 22:43:42 +0300

    Added all files and folders

---
+++ Arabic Speech Recognition/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Arabic Speech Recognition
+//
+//  Created by Omar Droubi on 4/26/20.
+//  Copyright © 2020 Omar Droubi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Speech
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
